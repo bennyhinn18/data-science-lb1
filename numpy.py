585f65561d389f08e10253a9e65257f1b253e88d
Bennyhinn18 <137780675+Bennyhinn18@users.noreply.github.com>
2023-10-11 13:47:50 +0530

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -1,7 +1,20 @@
-import numpy as np
-array = np.arange(20).reshape(5, 4)
-print(array)
-print()
-print(np.argmax(array))
-print(np.argmax(array, axis=1))
-print(np.argmax(array, axis=0))
+import numpy as np 
+
+# Creating array object 
+arr = np.array( [[ 1, 2, 3], 
+				[ 4, 2, 5]] ) 
+
+# Printing type of arr object 
+print("Array is of type: ", type(arr)) 
+
+# Printing array dimensions (axes) 
+print("No. of dimensions: ", arr.ndim) 
+
+# Printing shape of array 
+print("Shape of array: ", arr.shape) 
+
+# Printing size (total number of elements) of array 
+print("Size of array: ", arr.size) 
+
+# Printing type of elements in array 
+print("Array stores elements of type: ", arr.dtype) 
